e7eed67ca82e065cf39cb7e176eaaddddbf23ddf
Damsmh <shmychkov210605@gmail.com>
2025-11-26 22:02:10 +0300

    Fix Notification Entity

--- FakeObsidian.Domain/Entities/Notification.cs
+++ FakeObsidian.Domain/Entities/Notification.cs
@@ -2,7 +2,7 @@
 {
     public class Notification
     {
-        public string Id = Guid.NewGuid().ToString();
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string UserId { get; set; }
         public AppUser User { get; set; }
         public string Content { get; set; }
